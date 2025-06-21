4824e9de28a03635ff9313586b572f3a1c575949
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-21 21:35:10 +0600

    Offer part done

--- src/components/OfferBanner/OfferBanner.jsx
+++ src/components/OfferBanner/OfferBanner.jsx
@@ -15,7 +15,7 @@ const OfferBanner = () => {
 				<p className="text-[#00FF66] font-primary font-semibold leading-5  ">
 					Categories
 				</p>
-				<h3 className="font-secondary leading-[60px] tracking-[4%] font-semibold text-secondary my-8 max-w-[443px] ">
+				<h3 className="font-secondary leading-[60px] tracking-[4%] font-semibold text-[48px] text-secondary my-8 max-w-[443px] ">
 					Enhance Your Music Experience
 				</h3>
 				<div>
@@ -26,6 +26,13 @@ const OfferBanner = () => {
 					/>
 					
 				</div>
+        <div className="mt-10">
+          <a href="#">
+            <button className="font-primary font-medium leading-6 text-secondary py-4 px-[48px] bg-[#00FF66]">
+              Buy Now!
+            </button>
+          </a>
+        </div>
 			</div>
 		</Container>
 	);
