7bd0c3584c809aa6be21ba685749e73773f7b663
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-22 17:49:51 +0600

    Products Headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Home from './components/pages/Home'
+import Products from './components/pages/Products'
 
 function App() {
 
   return (
     <>
       <Home />
+      <Products />
     </>
   )
 }
