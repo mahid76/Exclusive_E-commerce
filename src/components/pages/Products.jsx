855099866db497161fb813e694af9b2fb878a18f
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-22 17:50:51 +0600

    Products Navbar

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Navbar from '../Navbar/Navbar'
 
 const Products = () => {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   )
 }
